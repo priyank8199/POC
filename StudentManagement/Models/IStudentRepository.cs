17eacab686bae623fd74f5208cb12e22db446568
Priyank Patel <pr371749@dal.ca>
2023-09-27 22:58:44 -0300

    Added Readme

---
+++ StudentManagement/Models/IStudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public interface IStudentRepository
+    {
+        Student GetStudent(int Id);
+        IEnumerable<Student> GetAllStudent();
+        Student Add(Student student);
+        Student Update(Student studentupdate);
+        Student Delete(Student student);
+       /* Task Delete(string id);
+        object Delete(Task user);*/
+    }
+}
---
+++ StudentManagement/Models/SqlStudentRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        private readonly StudentDbContext _context;
+        private readonly ILogger<SqlStudentRepository> _logger;
+
+        public SqlStudentRepository(StudentDbContext context,
+                                    ILogger<SqlStudentRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        public Student Add(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return student;
+        }
+
+        public Student Delete(Student student)
+        {
+            
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+            
+            return student;
+        }
+
+       /* public object Delete(Task user)
+        {
+            Student student = _context.Students.Find(user);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+            }
+            return student;
+
+        }*/
+
+       /* public Task Delete(string id)
+        {
+            throw new NotImplementedException();
+        }*/
+
+        public IEnumerable<Student> GetAllStudent()
+        {
+            return _context.Students;
+        }
+
+        public Student GetStudent(int Id)
+        {
+            var obj = _context.Students.Find(Id);
+            return obj;
+        }
+
+        public Student Update(Student studentupdate)
+        {
+            var student = _context.Students.Attach(studentupdate);
+            student.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            return studentupdate;
+        }
+    }
+}
